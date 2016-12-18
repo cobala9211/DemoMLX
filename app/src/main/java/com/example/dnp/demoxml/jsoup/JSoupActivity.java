769c5990cabd768489b8ad7de8aef49037011864
cobala9211 <dnphuong9211@gmail.com>
2016-12-19 00:07:48 +0700

    add change feature parsing xml jsoup

--- app/src/main/java/com/example/dnp/demoxml/jsoup/JSoupActivity.java
+++ app/src/main/java/com/example/dnp/demoxml/jsoup/JSoupActivity.java
@@ -3,6 +3,7 @@ package com.example.dnp.demoxml.jsoup;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.example.dnp.demoxml.R;
 
@@ -11,9 +12,14 @@ import com.example.dnp.demoxml.R;
  */
 
 public class JSoupActivity extends AppCompatActivity {
+    private static final String URL = "http://600tuvungtoeic.com/";
+    private static final String TAG = "JSoupActivity";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jsoup);
+        new XmlHandlerJSoup(this).execute(URL);
+        Log.d(TAG, "onCreate: ");
     }
 }
--- app/src/main/java/com/example/dnp/demoxml/jsoup/XmlHandlerJsoup.java
+++ app/src/main/java/com/example/dnp/demoxml/jsoup/XmlHandlerJsoup.java
@@ -1,17 +1,71 @@
 package com.example.dnp.demoxml.jsoup;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 /**
  * Created by DNP on 12/18/2016.
  */
 
-public class XmlHandlerJsoup extends AsyncTask<String, Void, Void> {
-    public XmlHandlerJsoup(){
+public class XmlHandlerJSoup extends AsyncTask<String, Void, Void> {
+    private Context mContext;
+    private ProgressDialog mDialog;
 
+    public XmlHandlerJSoup(Context context) {
+        this.mContext = context;
     }
+
     @Override
-    protected Void doInBackground(String... strings) {
+    protected void onPreExecute() {
+        super.onPreExecute();
+        mDialog = new ProgressDialog(mContext);
+        mDialog.setTitle("Parsing xml android by jsoup");
+        mDialog.setMessage("Loading...");
+        mDialog.setIndeterminate(true);
+        mDialog.show();
+    }
+
+    @Override
+    protected Void doInBackground(String... url) {
+        Document document = null;
+        try {
+            document = (Document) Jsoup.connect(url[0]).get();
+            if (document != null) {
+                //get gallery-time
+                Elements subElements = document.select("div.gallery-item");
+                if (subElements != null && subElements.size() > 0) {
+                    for (Element element : subElements) {
+                        Element titleSubject = element.getElementsByTag("h3").first();
+                        Element imgSubject = element.getElementsByTag("img").first();
+                        if (titleSubject != null) {
+                            String title = titleSubject.text();
+                            Log.d("TAG111", "doInBackground: " + title);
+                        }
+                        if (imgSubject != null) {
+                            String src = imgSubject.attr("src");
+                            Log.d("TAG111", "doInBackground: " + src);
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        mDialog.dismiss();
     }
 }
